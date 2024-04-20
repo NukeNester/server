ca33175133a25dade81953cbd20aee6a450d0bbd
Eric Chen <echen9870@gmail.com>
2024-04-19 22:00:03 -0400

    Refactored API calls

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
     organizationName: { type: String, required: true },
     dateOrdered: { type: Date, required: true }, 
-    wasteType: { type: Date, required: true }, 
+    wasteType: { type: String, required: true }, 
     quantity: { type: Number, required: true },
     timeToDecay: { type: Date, required: true },
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true }
 });
+
 
 const Order = mongoose.model('Order', orderSchema, 'orders');
 
